55c644d52400680942a52be3a05cae8a9c25dbae
sachin senal <127573781+sachinsenal0x64@users.noreply.github.com>
2023-12-15 03:14:50 +0000

    Delete routes.js

--- routes.js
+++
@@ -1,8 +0,0 @@
-// This file was added by edgio init.
-// You should commit this file to source control.
-
-import { Router } from "@edgio/core/router";
-
-export default new Router()
-  // Create serveStatic route for each file in the folder public with a cache-control header of 's-maxage=315360000'
-  .static("public");
